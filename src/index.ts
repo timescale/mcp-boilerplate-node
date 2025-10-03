666d094447eea2ae5df6c940790f63f9a970695c
Matthew Peveler <mpeveler@tigerdata.com>
2025-10-03 10:48:00 -0600

    fix: actually export things from package.json
    Signed-off-by: Matthew Peveler <mpeveler@tigerdata.com>

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { cliEntrypoint } from './cliEntrypoint.js';
+export { httpServerFactory } from './httpServer.js';
+export { log } from './logger.js';
+export { stdioServerFactory } from './stdio.js';
+export { type ApiFactory } from './types.js';
+export { StatusError } from './StatusError.js';
+export { type AdditionalSetupArgs } from './mcpServer.js';
