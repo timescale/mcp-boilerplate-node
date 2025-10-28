c076021ec3539b21d3a5cd1e28ccec122e76fc1a
Justin Murray <justin@timescale.com>
2025-10-28 13:56:12 -0400

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export { stdioServerFactory } from './stdio.js';
 export { type ApiFactory } from './types.js';
 export { StatusError } from './StatusError.js';
 export { type AdditionalSetupArgs } from './mcpServer.js';
+export { withSpan, addAiResultToSpan } from './tracing.js';
+export { registerExitHandlers } from './registerExitHandlers.js';
