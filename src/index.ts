974351ff860e1f63b85a17b961a5b7f2cafda8bd
Greg Saab <greg@timescale.com>
2026-06-29 11:56:27 -0400

    remove migrate export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export { cliEntrypoint } from './cliEntrypoint.js';
 export { httpServerFactory } from './httpServer.js';
 export { log } from './logger.js';
 export type { AdditionalSetupArgs } from './mcpServer.js';
-export { createMigrator } from './migrate.js';
 export { registerExitHandlers } from './registerExitHandlers.js';
 export { StatusError } from './StatusError.js';
 export { stdioServerFactory } from './stdio.js';
