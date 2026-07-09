b6c8b4ceb81a1e3fcf104a627a0880a0423b9b2a
Greg Saab <greg@timescale.com>
2026-07-09 15:34:22 -0400

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { ArrayStore, Store } from './store.js';
 export { cliEntrypoint } from './cliEntrypoint.js';
 export { httpServerFactory } from './httpServer.js';
 export { log } from './logger.js';
@@ -6,6 +5,7 @@ export type { AdditionalSetupArgs } from './mcpServer.js';
 export { registerExitHandlers } from './registerExitHandlers.js';
 export { StatusError } from './StatusError.js';
 export { stdioServerFactory } from './stdio.js';
+export { ArrayStore, Store } from './store.js';
 export { addAiResultToSpan, withSpan } from './tracing.js';
 export type {
   ApiFactory,
